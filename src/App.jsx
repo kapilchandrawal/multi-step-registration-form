bde0554f82d9753e2b4e1be33ea498575862a5cc
kapilchandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2026-09-18 19:56:10 +0530

    create initial registration form UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-
+import RegistrationForm from "./forms/RegistrationForm";
 function App() {
-  return <h1 className="text-4xl font-bold">Multi-Step Registration Form</h1>;
+  return (
+    <>
+      {/* <h1 className="text-4xl font-bold">Multi-Step Registration Form</h1>; */}
+      <RegistrationForm />
+    </>
+  );
 }
 
 export default App;
